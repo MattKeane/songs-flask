a92ead1dc69ae21994295662b379facd357e33f1
Matt Keane <mattpkeane@gmail.com>
2021-05-09 12:42:18 -0500

    added register user route

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ DEBUG = True
 app = Flask(__name__)
 
 from resources.artists import artists
+from resources.auth import auth
 app.register_blueprint(artists, url_prefix='/api/v1/artists')
+app.register_blueprint(auth, url_prefix='/api/v1/auth')
 
 @app.route('/')
 def test():
---
+++ resources/auth.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
+from flask_bcrypt import generate_password_hash
+
+import models
+
+auth = Blueprint('auth', 'auth')
+
+@auth.route('/register', methods=['POST'])
+def register_user():
+	payload = request.get_json()
+	try:
+		models.User.get(models.User.email == payload['email'])
+		return jsonify(
+			message='User with that email already exists',
+			status=401), 401
+	except models.DoesNotExist:
+		try:
+			models.User.get(models.User.username == payload['username'])
+			return jsonify(
+				message='User with that username already exists',
+				status=401), 401
+		except models.DoesNotExist:
+			payload['password'] = generate_password_hash(payload['password'])
+			created_user = models.User.create(**payload)
+			user_dict = model_to_dict(created_user)
+			user_dict.pop('password')
+			return jsonify(
+				data=user_dict,
+				message='User succesfully created',
+				status=201), 201
