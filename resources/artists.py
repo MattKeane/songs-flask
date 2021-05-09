116b468f27d1d3420912a939398d11a5e1e9552a
Matt Keane <mattpkeane@gmail.com>
2021-05-08 19:35:04 -0500

    added artist index route

--- resources/artists.py
+++ resources/artists.py
@@ -5,6 +5,15 @@ import models
 
 artists = Blueprint('artists', 'artists')
 
+@artists.route('/', methods=['GET'])
+def get_all_artists():
+	all_artists = models.Artist.select()
+	artist_dicts = [model_to_dict(artist) for artist in all_artists]
+	return jsonify(
+		data=artist_dicts,
+		message=f'Returned {len(artist_dicts)} artists.',
+		status=200), 200
+
 @artists.route('/', methods=['POST'])
 def create_new_artist():
 	payload = request.get_json()
