44f5081558d279d14d0ef05120584ab15cd5a12a
Matt Keane <mattpkeane@gmail.com>
2021-05-10 13:29:24 -0500

    added get all songs route

--- resources/songs.py
+++ resources/songs.py
@@ -8,7 +8,7 @@ songs = Blueprint('songs', 'songs')
 
 @songs.route('/', methods=['POST'])
 @login_required
-def get_all_songs():
+def add_new_song():
     payload = request.get_json()
     payload['added_by'] = current_user.id
     created_song = models.Song.create(**payload)
@@ -19,3 +19,17 @@ def get_all_songs():
         data=song_dict,
         message='Successfully created song',
         status=201), 201
+
+@songs.route('/', methods=['GET'])
+def get_all_songs():
+    all_songs = models.Song.select()
+    song_dicts = []
+    for song in all_songs:
+        song_dict = model_to_dict(song)
+        song_dict['added_by'].pop('password')
+        song_dict['artist'].pop('added_by')
+        song_dicts.append(song_dict)
+    return jsonify(
+        data=song_dicts,
+        message=f'Returned {len(song_dicts)} songs',
+        status=200), 200
